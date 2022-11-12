0137c96f7cbc13477424d3e24f49c43f08cf73e9
victor <nwosuvictor54@gmail.com>
2022-11-12 14:49:26 +0400

    bug: fixed cober images for the cards

--- src/place_to_stay.js
+++ src/place_to_stay.js
@@ -3,12 +3,13 @@ import Navbar from './components/navbar'
 // import Card from './components/card'
 import Footer from './components/footer'
 import Sec_nav from './components/sec_nav'
+import Card from './components/card'
 import data from './card_imgs'
 
 export default function Place_to_stay() {
     const cards = data.map(item =>{
         return (
-            {...item}
+            <Card images={item.images} />
         );
     })
 
